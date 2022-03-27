bf3c1d721693cc01ac11b941b853977e37bfd714
lehirschy <edensjunk@yahoo.com>
2022-03-27 15:59:37 -0500

    Shopping Cart

--- src/pages/shoppingcart.vue
+++ src/pages/shoppingcart.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen-nonav">
-    <h2 class="text-6xl tracking-tighter font-extralight">About Page</h2>
+    <h2 class="text-6xl tracking-tighter font-extralight">Shopping Cart</h2>
     <img src="../assets/Pay.png" alt="Pay" />
   </div>
 </template>
