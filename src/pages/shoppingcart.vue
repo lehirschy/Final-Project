52c5854a6739328bf16aca8d9b53718e48d46fbd
lehirschy <edensjunk@yahoo.com>
2022-04-11 14:44:31 -0500

    pic

--- src/pages/shoppingcart.vue
+++ src/pages/shoppingcart.vue
@@ -3,6 +3,6 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen-nonav">
     <h2 class="text-6xl tracking-tighter font-extralight">Shopping Cart</h2>
-    <img src="../assets/Pay.png" alt="Pay" />
+    <img src="C:\Users\edenh\OneDrive\Documents\Spring 2022\Web Applications Class\Projects\Final Project\Final-Project\src\assets" alt="Pay" />
   </div>
 </template>
