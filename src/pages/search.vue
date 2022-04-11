8059657e5df0745740423a2fd2adad4d78f1673e
lehirschy <edensjunk@yahoo.com>
2022-04-11 14:30:46 -0500

    milestone 2

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="flex flex-col items-center justify-center space-y-16 min-h-screen-nonav">
         <h2 class="text-6xl tracking-tighter font-extralight">
-            About Page
+            Search Page
         </h2>
         <h2>
             <UndrawSearch primary-color='#41B883' height='100px'/>
