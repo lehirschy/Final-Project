570f8256a7b5c67560b3339dc34defbc74af6a78
lehirschy <edensjunk@yahoo.com>
2022-04-11 15:00:09 -0500

    firebase

--- src/composable/useFirebase.js
+++ src/composable/useFirebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 
 import {getAuth} from "firebase/auth";
 
-import { firebaseConfig } from "/src/firebaseConfig.js";
+import { firebaseConfig } from "./src/firebaseConfig.js";
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
